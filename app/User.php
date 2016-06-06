3e190fe13d6e0ba5d90ffd9114bceacc39f1288f
Rob Grieshaber <rob@lgd-it.us>
2016-06-06 16:06:50 -0500

    Reverting to master and will put column sorting on a wish list

--- app/User.php
+++ app/User.php
@@ -14,7 +14,7 @@ class User extends Authenticatable
     protected $fillable = [
         'username', 'email', 'password', 'first_name', 'last_name', 'role_id', 'phone',
     ];
-
+	
     /**
      * The attributes that should be hidden for arrays.
      *
