64fda263bc823cdf60fabd420bb3c82139b99e18
Rob Grieshaber <rob@lgd-it.us>
2016-09-21 16:48:29 -0500

    Trying different things with availUsers, as a Map or something else. Need to have an efficient means of sorting and searching

--- public/js/project13Create.js
+++ public/js/project13Create.js
@@ -1,27 +1,21 @@
 
-
-var availUsers = [];
+var availUsers = new Map();
 var usedUsers = [];
 var selectControls = $("select[id!='organization']");
-
 $('#organization').on('change', function (e) {
-	// When organization changes, need to initialize the user selects
+// When organization changes, need to initialize the user selects
 	initSelectControls(e);
 });
-
 $("select[id!='organization']").on('change', function (e) {
-	// When any of the user selects changes, need to update availUsers and usedUsers
+// When any of the user selects changes, need to update availUsers and usedUsers
 	updateSelectControls(e);
 });
-
 function initSelectControls(e) {
 	var control;
 	var org_id = e.target.value;
-
 	$.get('/org-users?org_id=' + org_id, function (data) {
 		// Initialize availUsers array
 		initAvailUsers(data);
-
 		$.each(selectControls, function (index, controlId) {
 			control = $('#' + controlId.name);
 			control.empty();
@@ -49,15 +43,14 @@ function updateAvailUsers(data) {
 }
 
 function initAvailUsers(data) {
-	// Ensure that availItems is empty
+// Ensure that availItems is empty
 	var newUser;
 	for (var i = 0; i < data.length; i++) {
-		newUser = {
-			id: data[i].id,
-			name: data[i].last_name + ', ' + data[i].first_name
-		};
-		availUsers.push(newUser);
+		availUsers.set(data[i].id, data[i].last_name + ', ' + data[i].first_name);
 	}
+	availUsers.forEach(function (value, key) {
+		console.log(key + " = " + value);
+	}, availUsers);
 }
 
 	
