b6d4130adcd98b8f3c2187a9e2fd635d836de9f0
Rob Grieshaber <rob@lgd-it.us>
2016-09-22 14:15:48 -0500

    Still workiing on updating availUsers and usedUsers. When a select control is changed back to [Select] the User is removed from usedUsers but not added back to availUsers

--- public/js/project13Create.js
+++ public/js/project13Create.js
@@ -10,27 +10,41 @@ var availUsers = [];
  * usedUsers:	Stores Users for the Organization that have been selected
  *		fields:	selectId	The id (and name) of the select control that has this User selected
  *				userId		The id of the User from the database
+ *				userName	The name of the User, saved to ease adding back to availUsers if necessary
  */
 var usedUsers = [];
 
-// jQuery generated array of all select controls that don't include organization
+
+/*
+ * jQuery generated array of all select controls that don't include organization
+ */
 var selectControls = $("select[id!='organization']");
 
-// onChange handler for the organization select control
+
+/*
+ * onChange handler for the organization select control
+ */
 $('#organization').on('change', function (e) {
 // When organization changes, need to initialize the user selects
-	initSelectControls(e);
+	initSelectControls(parseInt(e.target.value));
 });
 
-// onChange handler for all select controls except organization
+
+/*
+ *  onChange handler for all select controls except organization
+ */
 $("select[id!='organization']").on('change', function (e) {
 // When any of the user selects changes, need to update availUsers and usedUsers
-	updateSelectControls(e);
+	updateSelectControls(e.target);
 });
-function initSelectControls(e) {
+
+
+/*
+ * Initialize the select controls, as when a different organization is selected
+ */
+function initSelectControls(orgId) {
 	var control;
-	var org_id = e.target.value;
-	$.get('/org-users?org_id=' + org_id, function (data) {
+	$.get('/org-users?org_id=' + orgId, function (data) {
 		// Initialize availUsers array
 		initAvailUsers(data);
 		$.each(selectControls, function (index, controlId) {
@@ -38,54 +52,183 @@ function initSelectControls(e) {
 			control.empty();
 			control.append('<option value="0">[Select]</option>');
 			availUsers.forEach(function (user) {
-				control.append('<option value="' + user.id + '">' + user.name + '</option>');
+				control.append('<option value=' + user.id + '>' + user.name + '</option>');
 			});
 		});
 	});
 }
 
-function updateSelectControls(e) {
-//	console.log('Name:' + e.target.name);
-//	console.log('Value: ' + e.target.value);
+
+/*
+ * Update select controls, as when a selection changes and all others must be updated
+ */
+function updateSelectControls(select) {
+	// First update availUsers and usedUsers
+	updateCollections(select);
+
+	
+}
+
+/*
+ * Update availUsers and usedUsers, to be used when updating the select controls
+ */
+function updateCollections(select) {
+	var user;
+	// First check whether selection was changed to '[Select]'
+	if (parseInt(select.value) === 0) {
+		// Restore the User referenced by 'select' to availUsers
+		user = restoreUsedUser(select.name).user;
+		addAvailUser(user);
+	}
 	// Check whether the select control is already in usedUsers
-	if (!selectIsUsed(e)) {
-		// Save selected user to usedUsers
-		usedUsers.push({
-			selectId:	e.target.name,
-			userId:		e.target.value
-		});
-		// Remove selected user from availUsers
-		
+	else if (!selectIsUsed(select.name)) {
+		// Move the User from availUsers to UsedUsers
+		addUsedUser(select);
+	}
+	// Otherwise this select control's value was just change
+	// Remove the existing User from usedUsers, restore it to availUsers, then add the selected User to usedUsers
+	else {
+		user = restoreUsedUser(select.name);
+		addAvailUser(user);
+		addUsedUser(select);
 	}
 }
 
+/*
+ * Update availUsers, removing or adding when one is selected in a select control
+ */
 function updateAvailUsers(data) {
 	if (availUsers.length === 0) {
 		initAvailUsers(data);
 	}
 	else {
-		
+
 	}
 
 }
 
+/*
+ * Initialize availUsers, as when the organization changes
+ */
 function initAvailUsers(data) {
 // Ensure that availItems is empty
 	availUsers = [];
-	var newUser;
 	for (var i = 0; i < data.length; i++) {
-		newUser = {
+		availUsers.push({
 			id: data[i].id,
 			name: data[i].last_name + ', ' + data[i].first_name
-		};
-		availUsers.push(newUser);
+		});
 	}
 }
 
-function selectIsUsed(data) {
-	// Returns true if data.id is found in usedUsers
-	usedUsers.forEach(function(item, index, array) {
-		
-	});
+/*
+ * Add the User referenced in 'data' to availUsers and re-sort
+ */
+function addAvailUser(user) {
+
 }
+
+/*
+ * Remove the User referenced by 'id' from availUsers and re-sort
+ */
+function removeAvailUser(id) {
+	var removedUser;
+	// TODO change this to a for loop to enable a break
+	availUsers.forEach(function (user) {
+		if (user.id === id) {
+			var pos = availUsers.indexOf(user);
+			removedUser = availUsers.splice(pos, 1);
+			
+		}
+	});
+
+	sortAvailUsers();
+
+	return removedUser;
+}
+
+function sortAvailUsers() {
+	availUsers.sort(compareAvailUsers);
+}
+
+
+/*
+ * Compare function used by sortAvailUsers
+ */
+function compareAvailUsers(a, b) {
+	if (a.name < b.name) {
+		return -1;
+	}
+	if (a.name > b.name) {
+		return 1;
+	}
+	return 0;
+}
+
+/*
+ * Get the name from availUsers using the User ID
+ */
+function getAvailUserName(userId) {
+	availUsers.forEach(function (user) {
+		if (user.id === userId) {
+			return user.name;
+		}
+	});
+	return '';
+}
+
+/*
+ * Determine whether the select control referenced by 'data' is in usedUsers
+ */
+function selectIsUsed(selectName) {
+	// Returns true if select control's name is found in usedUsers
+
+	usedUsers.forEach(function (usedUser) {
+		if (usedUser.selectName === selectName) {
+			return true;
+		}
+	});
+	return false;
+}
+
+
+/*
+ * Remove the selected User from availUsers and add to UsedUsers
+ */
+function addUsedUser(select) {
+	// Remove selected user from availUsers ...
+	// select.value is id of User
+	var user = removeAvailUser(parseInt(select.value));
+
+	// ... and save selected user to usedUsers ...
+	usedUsers.push({
+		selectName: select.name,
+		usedUser: user
+	});
+
+}
+
+
+/*
+ * Remove selected User from usedUsers and return the User
+ */
+function removeUsedUser(selectName) {
+	var removedUser;
+	for (var i = 0; i < usedUsers.length; i++) {
+		if (usedUsers[i].selectName === selectName) {
+			removedUser = usedUsers.splice(i, 1);
+		}
+	}
+
+	return removedUser;
+}
+
+
+/*
+ * Removes the User referenced by 'select' and restores it to availUsers
+ */
+function restoreUsedUser(selectName) {
+	user = removeUsedUser(selectName);
 	
+	return user;
+}
