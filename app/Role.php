213d434245def7bc80b4af0eea0522fbda90a065
Rob Grieshaber <rob@lgd-it.us>
2017-05-10 02:42:54 -0500

    Working in Laratrust authorization package

--- app/Role.php
+++ app/Role.php
@@ -2,29 +2,9 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Laratrust\LaratrustRole;
 
-class Role extends Model
+class Role extends LaratrustRole
 {
-    protected $table = 'roles';
-	
-	protected $fillable = ['role'];
-	
-	protected $hidden = [];
-	
-	public function users() {
-		return $this->hasMany('App\User');
-	}
-	
-	public static function getRoleId($role) {
-		$roles = Role::where('role', $role)->get();
-		$roleId = 0;
-		
-		foreach($roles as $r) {
-			$roleId = $r->id;
-		}
-		
-		return $roleId;
-	}
-
+    //
 }
