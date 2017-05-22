c3b634d45be2220e170fca884182aa155dd09f3c
Rob Grieshaber <rob@lgd-it.us>
2017-05-22 04:06:32 -0500

    Renamed UsersTableSeeder in autoload_classmap Commented out all seeders except MembersTableSeeder in DatabaseSeeder Previous seeding didn't successfully seed Roles with first role_id = 0, so manually edited the 7 rows and backed up at 1705220405

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,18 +12,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        DB::statement('ALTER TABLE roles AUTO_INCREMENT=0');
-        DB::table('roles')->insert([
-			['name' => 'none', 'display_name' => '[None]'],
-			['name' => 'district_admin', 'display_name' => 'District Administrator'],
-			['name' => 'org_admin', 'display_name' => 'Organization Administrator'],
-			['name' => 'black_hat', 'display_name' => 'Black Hat'],
-			['name' => 'white_hat', 'display_name' => 'White Hat'],
-			['name' => 'blue_hat', 'display_name' => 'Blue Hat'],
-			['name' => 'yellow_hat', 'display_name' => 'Yellow Hat']
-		]);
-		$this->call(OrganizationsTableSeeder::class);
+//        DB::statement('ALTER TABLE roles AUTO_INCREMENT=0');
+//        DB::table('roles')->insert([
+//			['name' => 'none', 'display_name' => '[None]'],
+//			['name' => 'district_admin', 'display_name' => 'District Administrator'],
+//			['name' => 'org_admin', 'display_name' => 'Organization Administrator'],
+//			['name' => 'black_hat', 'display_name' => 'Black Hat'],
+//			['name' => 'white_hat', 'display_name' => 'White Hat'],
+//			['name' => 'blue_hat', 'display_name' => 'Blue Hat'],
+//			['name' => 'yellow_hat', 'display_name' => 'Yellow Hat']
+//		]);
+//		$this->call(OrganizationsTableSeeder::class);
 		$this->call(MembersTableSeeder::class);
-		$this->call(PermissionsTableSeeder::class);
+//		$this->call(PermissionsTableSeeder::class);
     }
 }
